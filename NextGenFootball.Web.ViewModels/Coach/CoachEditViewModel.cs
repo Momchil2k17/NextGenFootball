d1abe96fac4ed20009a9bc42e72d3489157c1b1a
Momchil Yankov <momchil200067@gmail.com>
2025-07-04 13:35:41 +0300

    Coach edit implemented

---
+++ NextGenFootball.Web.ViewModels/Coach/CoachEditViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenFootball.Web.ViewModels.Coach
+{
+    public class CoachEditViewModel : CoachCreateViewModel
+    {
+        public Guid Id { get; set; }
+        public Guid? ApplicationUserId { get; set; }
+
+    }
+}
