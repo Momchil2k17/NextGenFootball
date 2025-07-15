b11ed8c314bb8b7c03edfd44b65a15c6cfab621c
Momchil Yankov <momchil200067@gmail.com>
2025-07-15 23:20:40 +0300

    Added custom error pages

--- NextGenFootball.Web/Controllers/HomeController.cs
+++ NextGenFootball.Web/Controllers/HomeController.cs
@@ -24,9 +24,18 @@ namespace NextGenFootball.Web.Controllers
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult Error(int? statusCode)
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            switch (statusCode)
+            {
+                case 401:
+                case 403:
+                    return this.View("Forbidden");
+                case 404:
+                    return this.View("NotFoundError");
+                default:
+                    return this.View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
         }
     }
 }
