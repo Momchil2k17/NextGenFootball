ab04f50fc8c1b70c381c02071aaac7f036114723
Momchil Yankov <momchil200067@gmail.com>
2025-07-30 13:51:54 +0300

    Referee calendar added(functionality to be implemented)

---
+++ NextGenFootball.Web/Areas/Referee/Controllers/CalendarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NextGenFootball.Web.Areas.Referee.Controllers
+{
+    public class CalendarController : BaseRefereeController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ NextGenFootball.Web/wwwroot/js/referee-calendar.js
@@ -0,0 +1,23 @@
+﻿window.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('referee-calendar');
+    if (calendarEl) {
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+            initialView: 'dayGridMonth',
+            headerToolbar: {
+                left: 'prev,next',
+                center: 'title',
+                right: 'today' // today button on the right
+            },
+            height: 600,
+            events: [
+                // Demo event
+                { title: 'Match vs City', start: new Date().toISOString().slice(0, 10), color: '#198754' }
+            ],
+            selectable: true,
+            select: function (info) {
+                alert('Selected date: ' + info.startStr);
+            }
+        });
+        calendar.render();
+    }
+});
