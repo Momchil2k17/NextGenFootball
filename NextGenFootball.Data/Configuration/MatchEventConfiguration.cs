2d1698cf09cc60e3474bfd9cfed287b36e3bbcb5
Momchil Yankov <momchil200067@gmail.com>
2025-08-01 12:19:15 +0300

    Added query filters on MatchEvent and MatchReport to stop warning for global query filters from the Match

---
+++ NextGenFootball.Data/Configuration/MatchEventConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NextGenFootball.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenFootball.Data.Configuration
+{
+    public class MatchEventConfiguration : IEntityTypeConfiguration<MatchEvent>
+    {
+        public void Configure(EntityTypeBuilder<MatchEvent> entity)
+        {
+            entity
+                .HasKey(me => me.Id);
+
+            entity
+                .HasQueryFilter(me => !me.MatchReport!.Match!.IsDeleted);
+        }
+    }
+}
---
+++ NextGenFootball.Data/Configuration/MatchReportConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NextGenFootball.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextGenFootball.Data.Configuration
+{
+    public class MatchReportConfiguration : IEntityTypeConfiguration<MatchReport>
+    {
+        public void Configure(EntityTypeBuilder<MatchReport> entity)
+        {
+            entity
+                .HasKey(mr => mr.Id);
+
+            entity
+                .HasQueryFilter(mr => !mr.Match!.IsDeleted);
+        }
+    }
+}
