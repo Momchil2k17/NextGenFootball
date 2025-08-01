872545adbc67f6c697254c18782d57689bdfdba4
Momchil Yankov <momchil200067@gmail.com>
2025-08-01 16:33:39 +0300

    Leagues seeded

--- NextGenFootball.Data/Configuration/LeagueConfiguration.cs
+++ NextGenFootball.Data/Configuration/LeagueConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NextGenFootball.Data.Common.Enums;
 using NextGenFootball.Data.Models;
 using System;
 using System.Collections.Generic;
@@ -52,6 +53,49 @@ namespace NextGenFootball.Data.Configuration
 
             entity
                 .HasQueryFilter(l => l.IsDeleted == false);
+
+            entity
+                .HasData(this.SeedLeagues());
+        }
+        public IEnumerable<League> SeedLeagues()
+        {
+            List<League> leagues = new List<League>
+            {
+                new League
+                {
+                    Id = 1,
+                    Name = "Елитна юношеска група до 15 г.",
+                    AgeGroup = "U15",
+                    Description = "",
+                    ImageUrl = "https://marketplace.canva.com/EAGFNmKiY9s/1/0/1600w/canva-blue-soccer-sports-logo-rQrjayPQsF0.jpg",
+                    Region = Region.България,
+                    IsDeleted = false,
+                    SeasonId = 1
+                },
+                new League
+                {
+                    Id = 2,
+                    Name = "Елитна юношеска група до 16г.",
+                    AgeGroup = "U16",
+                    Description = "",
+                    ImageUrl = "https://i.pinimg.com/736x/d4/c7/65/d4c765fb353b3901676a1bdbda3f9706.jpg",
+                    Region = Region.България,
+                    IsDeleted = false,
+                    SeasonId = 1
+                },
+                new League
+                {
+                    Id = 3,
+                    Name = "Елитна юношеска група до 17г.",
+                    AgeGroup = "U17",
+                    Description = "",
+                    ImageUrl = "https://img.freepik.com/premium-vector/ball-with-three-spotting-stripe-football-league-logo_8296-13.jpg",
+                    Region = Region.България,
+                    IsDeleted = false,
+                    SeasonId = 1
+                },
+            };
+            return leagues;
         }
     }
 }
---
+++ NextGenFootball.Data/Migrations/20250801133146_SeedLeagues.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NextGenFootball.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedLeagues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Leagues",
+                columns: new[] { "Id", "AgeGroup", "Description", "ImageUrl", "Name", "Region", "SeasonId" },
+                values: new object[,]
+                {
+                    { 1, "U15", "", "https://marketplace.canva.com/EAGFNmKiY9s/1/0/1600w/canva-blue-soccer-sports-logo-rQrjayPQsF0.jpg", "Елитна юношеска група до 15 г.", 5, 1 },
+                    { 2, "U16", "", "https://i.pinimg.com/736x/d4/c7/65/d4c765fb353b3901676a1bdbda3f9706.jpg", "Елитна юношеска група до 16г.", 5, 1 },
+                    { 3, "U17", "", "https://img.freepik.com/premium-vector/ball-with-three-spotting-stripe-football-league-logo_8296-13.jpg", "Елитна юношеска група до 17г.", 5, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Leagues",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
