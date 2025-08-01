f765beb196d188ea95beecff0130b15d11fdfd75
Momchil Yankov <momchil200067@gmail.com>
2025-08-01 15:43:33 +0300

    Seed seasons

--- NextGenFootball.Data/Configuration/SeasonConfiguration.cs
+++ NextGenFootball.Data/Configuration/SeasonConfiguration.cs
@@ -28,6 +28,42 @@ namespace NextGenFootball.Data.Configuration
 
             entity
                 .HasQueryFilter(s => s.IsDeleted==false);
+
+            entity
+                .HasData(this.SeedSeasons());
+        }
+        private IEnumerable<Season> SeedSeasons()
+        {
+            return new List<Season>
+            {
+                new Season
+                {
+                    Id = 1,
+                    Name = "2025/2026",
+                    StartDate = new DateTime(2025, 8, 1),
+                    EndDate = new DateTime(2026, 5, 31),
+                    IsCurrent=DateTime.Now>= new DateTime(2025, 8, 1) && DateTime.Now <= new DateTime(2026, 5, 31),
+                    IsDeleted = false
+                },
+                new Season
+                {
+                    Id = 2,
+                    Name = "2026/2027",
+                    StartDate = new DateTime(2026, 8, 1),
+                    EndDate = new DateTime(2027, 5, 31),
+                    IsCurrent=DateTime.Now>= new DateTime(2026, 8, 1) && DateTime.Now <= new DateTime(2027, 5, 31),
+                    IsDeleted = false
+                },
+                new Season
+                {
+                    Id = 3,
+                    Name = "2027/2028",
+                    StartDate = new DateTime(2027, 8, 1),
+                    EndDate = new DateTime(2028, 5, 31),
+                    IsCurrent=DateTime.Now>= new DateTime(2027, 8, 1) && DateTime.Now <= new DateTime(2028, 5, 31),
+                    IsDeleted = false
+                }
+            };
         }
     }
 }
---
+++ NextGenFootball.Data/Migrations/20250801124251_SeedSeasons.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NextGenFootball.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedSeasons : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Seasons",
+                columns: new[] { "Id", "EndDate", "IsCurrent", "Name", "StartDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2026, 5, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), true, "2025/2026", new DateTime(2025, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 2, new DateTime(2027, 5, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), false, "2026/2027", new DateTime(2026, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) },
+                    { 3, new DateTime(2028, 5, 31, 0, 0, 0, 0, DateTimeKind.Unspecified), false, "2027/2028", new DateTime(2027, 8, 1, 0, 0, 0, 0, DateTimeKind.Unspecified) }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Seasons",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Seasons",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Seasons",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
