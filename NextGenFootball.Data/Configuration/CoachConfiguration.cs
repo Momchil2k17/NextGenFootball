c37f35829ec3619d13321eb1d4b8a541e7913823
Momchil Yankov <momchil200067@gmail.com>
2025-08-04 21:53:19 +0300

    Coach seeding

--- NextGenFootball.Data/Configuration/CoachConfiguration.cs
+++ NextGenFootball.Data/Configuration/CoachConfiguration.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NextGenFootball.Data.Common.Enums;
 using NextGenFootball.Data.Models;
 using System;
 using System.Collections.Generic;
@@ -61,6 +62,52 @@ namespace NextGenFootball.Data.Configuration
             entity
                 .HasQueryFilter(p => p.IsDeleted == false);
 
+            entity
+                .HasData(this.SeedCoaches());
+
+        }
+        private IEnumerable<Coach> SeedCoaches()
+        {
+            List<Coach> coaches = new List<Coach>
+                {
+                    new Coach
+                    {
+                        Id = Guid.Parse("e6b7a0b8-9f40-4c26-9a9d-1a1e6b2f1a01"),
+                        ApplicationUserId = null,
+                        FirstName = "Jürgen",
+                        LastName = "Klopp",
+                        Role = CoachRole.HeadCoach,
+                        PhoneNumber = "+359888111222",
+                        TeamId = 1,
+                        ImageUrl = "https://img.a.transfermarkt.technology/portrait/big/118-1736865351.jpg?lm=1",
+                        IsDeleted = false
+                    },
+                    new Coach
+                    {
+                        Id = Guid.Parse("b2fa78d3-0e5d-46e2-bc2e-2a9f0d8f5c02"),
+                        ApplicationUserId = null,
+                        FirstName = "José",
+                        LastName = "Mourinho",
+                        Role = CoachRole.HeadCoach,
+                        PhoneNumber = "+359888333444",
+                        TeamId = 2,
+                        ImageUrl = "https://img.a.transfermarkt.technology/portrait/header/781-1717168225.jpg?lm=1",
+                        IsDeleted = false
+                    },
+                    new Coach
+                    {
+                        Id = Guid.Parse("5e8c2a51-8d6f-4013-bd8f-3fae16b2a803"),
+                        ApplicationUserId = null,
+                        FirstName = "Alex",
+                        LastName = "Ferguson",
+                        Role = CoachRole.HeadCoach,
+                        PhoneNumber = "+359888555666",
+                        TeamId = 3,
+                        ImageUrl = "https://img.a.transfermarkt.technology/portrait/header/_1344344740.jpg?lm=1",
+                        IsDeleted = false
+                    }
+                };
+            return coaches;
         }
     }
 }
---
+++ NextGenFootball.Data/Migrations/20250804185102_SeedCoaches.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NextGenFootball.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedCoaches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Coaches",
+                columns: new[] { "Id", "ApplicationUserId", "FirstName", "ImageUrl", "LastName", "PhoneNumber", "Role", "TeamId" },
+                values: new object[,]
+                {
+                    { new Guid("5e8c2a51-8d6f-4013-bd8f-3fae16b2a803"), null, "Alex", "https://img.a.transfermarkt.technology/portrait/header/_1344344740.jpg?lm=1", "Ferguson", "+359888555666", 1, 3 },
+                    { new Guid("b2fa78d3-0e5d-46e2-bc2e-2a9f0d8f5c02"), null, "José", "https://img.a.transfermarkt.technology/portrait/header/781-1717168225.jpg?lm=1", "Mourinho", "+359888333444", 1, 2 },
+                    { new Guid("e6b7a0b8-9f40-4c26-9a9d-1a1e6b2f1a01"), null, "Jürgen", "https://img.a.transfermarkt.technology/portrait/big/118-1736865351.jpg?lm=1", "Klopp", "+359888111222", 1, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Coaches",
+                keyColumn: "Id",
+                keyValue: new Guid("5e8c2a51-8d6f-4013-bd8f-3fae16b2a803"));
+
+            migrationBuilder.DeleteData(
+                table: "Coaches",
+                keyColumn: "Id",
+                keyValue: new Guid("b2fa78d3-0e5d-46e2-bc2e-2a9f0d8f5c02"));
+
+            migrationBuilder.DeleteData(
+                table: "Coaches",
+                keyColumn: "Id",
+                keyValue: new Guid("e6b7a0b8-9f40-4c26-9a9d-1a1e6b2f1a01"));
+        }
+    }
+}
