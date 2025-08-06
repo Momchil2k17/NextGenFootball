8f7db81403445f66cbf9f0b9155a99e07e60b2a9
Momchil Yankov <momchil200067@gmail.com>
2025-08-06 12:19:05 +0300

    Seeded some matches

--- NextGenFootball.Data/Configuration/MatchConfiguration.cs
+++ NextGenFootball.Data/Configuration/MatchConfiguration.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NextGenFootball.Data.Common.Enums;
 using NextGenFootball.Data.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using static NextGenFootball.Data.Common.EntityConstants.MatchValidationConstants;
 
 namespace NextGenFootball.Data.Configuration
@@ -90,6 +86,28 @@ namespace NextGenFootball.Data.Configuration
 
             entity
                 .HasQueryFilter(m => m.IsDeleted == false);
+            entity
+                .HasData(this.SeedMatches());
+        }
+        public IEnumerable<Match> SeedMatches()
+        {
+            var today = new DateTime(2025, 8, 6);
+            List<Match> matches = new List<Match>
+            {
+               new Match { Id = 1, HomeTeamId = 1, AwayTeamId = 2, StadiumId = 1, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 1, Date = today.AddDays(6).AddHours(14) },
+    new Match { Id = 2, HomeTeamId = 3, AwayTeamId = 4, StadiumId = 3, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 1, Date = today.AddDays(6).AddHours(15) },
+    new Match { Id = 3, HomeTeamId = 5, AwayTeamId = 6, StadiumId = 5, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 1, Date = today.AddDays(6).AddHours(16) },
+    new Match { Id = 4, HomeTeamId = 7, AwayTeamId = 8, StadiumId = 7, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 1, Date = today.AddDays(6).AddHours(17) },
+    new Match { Id = 5, HomeTeamId = 9, AwayTeamId = 10, StadiumId = 9, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 1, Date = today.AddDays(6).AddHours(18) },
+
+    // Round 2
+    new Match { Id = 6, HomeTeamId = 2, AwayTeamId = 3, StadiumId = 2, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 2, Date = today.AddDays(7).AddHours(14) },
+    new Match { Id = 7, HomeTeamId = 4, AwayTeamId = 5, StadiumId = 4, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 2, Date = today.AddDays(7).AddHours(15) },
+    new Match { Id = 8, HomeTeamId = 6, AwayTeamId = 7, StadiumId = 6, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 2, Date = today.AddDays(7).AddHours(16) },
+    new Match { Id = 9, HomeTeamId = 8, AwayTeamId = 9, StadiumId = 8, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 2, Date = today.AddDays(7).AddHours(17) },
+    new Match { Id = 10, HomeTeamId = 10, AwayTeamId = 1, StadiumId = 10, LeagueId = 1, Status = MatchStatus.Scheduled, Round = 2, Date = today.AddDays(7).AddHours(18) }
+            };
+            return matches;
         }
     }
 }
---
+++ NextGenFootball.Data/Migrations/20250806091352_SeedMatches.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NextGenFootball.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedMatches : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Matches",
+                columns: new[] { "Id", "AssistantReferee1Id", "AssistantReferee2Id", "AwayScore", "AwayTeamId", "Date", "HomeScore", "HomeTeamId", "LeagueId", "MatchReportId", "RefereeId", "Round", "StadiumId", "Status", "VideoUrl" },
+                values: new object[,]
+                {
+                    { 1L, null, null, null, 2, new DateTime(2025, 8, 12, 14, 0, 0, 0, DateTimeKind.Unspecified), null, 1, 1, null, null, 1, 1, 1, null },
+                    { 2L, null, null, null, 4, new DateTime(2025, 8, 12, 15, 0, 0, 0, DateTimeKind.Unspecified), null, 3, 1, null, null, 1, 3, 1, null },
+                    { 3L, null, null, null, 6, new DateTime(2025, 8, 12, 16, 0, 0, 0, DateTimeKind.Unspecified), null, 5, 1, null, null, 1, 5, 1, null },
+                    { 4L, null, null, null, 8, new DateTime(2025, 8, 12, 17, 0, 0, 0, DateTimeKind.Unspecified), null, 7, 1, null, null, 1, 7, 1, null },
+                    { 5L, null, null, null, 10, new DateTime(2025, 8, 12, 18, 0, 0, 0, DateTimeKind.Unspecified), null, 9, 1, null, null, 1, 9, 1, null },
+                    { 6L, null, null, null, 3, new DateTime(2025, 8, 13, 14, 0, 0, 0, DateTimeKind.Unspecified), null, 2, 1, null, null, 2, 2, 1, null },
+                    { 7L, null, null, null, 5, new DateTime(2025, 8, 13, 15, 0, 0, 0, DateTimeKind.Unspecified), null, 4, 1, null, null, 2, 4, 1, null },
+                    { 8L, null, null, null, 7, new DateTime(2025, 8, 13, 16, 0, 0, 0, DateTimeKind.Unspecified), null, 6, 1, null, null, 2, 6, 1, null },
+                    { 9L, null, null, null, 9, new DateTime(2025, 8, 13, 17, 0, 0, 0, DateTimeKind.Unspecified), null, 8, 1, null, null, 2, 8, 1, null },
+                    { 10L, null, null, null, 1, new DateTime(2025, 8, 13, 18, 0, 0, 0, DateTimeKind.Unspecified), null, 10, 1, null, null, 2, 10, 1, null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 1L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 2L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 3L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 4L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 5L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 6L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 7L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 8L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 9L);
+
+            migrationBuilder.DeleteData(
+                table: "Matches",
+                keyColumn: "Id",
+                keyValue: 10L);
+        }
+    }
+}
